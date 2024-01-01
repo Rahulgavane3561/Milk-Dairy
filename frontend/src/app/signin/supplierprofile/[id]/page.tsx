769bba2f38fa8fd79b92ad3ec54793783543b2e3
Rahulgavane8105 <rahulgavane607@gmail.com>
2024-01-01 11:32:58 +0530

    page of signin is modified

--- frontend/src/app/signin/supplierprofile/[id]/page.tsx
+++ frontend/src/app/signin/supplierprofile/[id]/page.tsx
@@ -14,7 +14,7 @@ function page() {
             <div className="card bg-primary text-white mb-4">
               <div className="card-body">Primary Card</div>
               <div className="card-footer d-flex align-items-center justify-content-between">
-                <a className="small text-white stretched-link" href="#">View Details</a>
+                <a className="small text-white stretched-link" href="#">Visit Details</a>
                 <div className="small text-white"><i className="fas fa-angle-right"></i></div>
               </div>
             </div>
